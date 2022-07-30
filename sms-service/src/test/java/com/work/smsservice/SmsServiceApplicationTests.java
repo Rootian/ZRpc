fe7e6c0962fc8f766d157e642b0db3d3642f4768
= <846587073@qq.com>
2022-07-30 14:35:58 -0400

    inital commit

---
+++ sms-service/src/test/java/com/work/smsservice/SmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.smsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
